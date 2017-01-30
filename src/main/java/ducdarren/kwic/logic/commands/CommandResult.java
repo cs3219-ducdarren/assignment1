5f21c613f83c24061e4c2f08005ebef5b530edbe
acuodancer <leminhduc74@gmail.com>
2017-01-30 19:36:11 +0800

    Add CommandResult class

---
+++ src/main/java/ducdarren/kwic/logic/commands/CommandResult.java
@@ -0,0 +1,16 @@
+package ducdarren.kwic.logic.commands;
+
+/**
+ * Result of a command in form of user feedback (string)
+ */
+
+public class CommandResult {
+
+    public final String userFeedBack;
+
+    public CommandResult(String userFeedBack) {
+        assert userFeedBack != null;
+        this.userFeedBack = userFeedBack;
+	}
+
+}
