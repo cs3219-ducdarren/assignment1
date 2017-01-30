8b6ab795f5d00b6aae3067d4dc5df90114f9e63b
acuodancer <leminhduc74@gmail.com>
2017-01-31 01:11:14 +0800

    Add wrapper class for titles list

--- src/ducdarren/kwic/model/TitlesList.java
+++ src/ducdarren/kwic/model/TitlesList.java
@@ -6,11 +6,11 @@ public class TitlesList {
 	
 	ArrayList<String[]> titles = new ArrayList<String[]>();
 	
-	private void addTitle(String[] title) {
+	protected void addTitle(String[] title) {
 		titles.add(title);
 	}
 	
-	private void addTitle(String title) {
+	protected void addTitle(String title) {
 		this.addTitle(tokenize(title));
 	}
 	
