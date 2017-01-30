2cc2c7fd6af9aade73c52b8a4c8735262a067a09
acuodancer <leminhduc74@gmail.com>
2017-01-30 23:07:02 +0800

    Remove Logic interface, repurpose LogicManager as new main logic class

--- src/main/java/ducdarren/kwic/logic/Logic.java
+++ src/main/java/ducdarren/kwic/logic/Logic.java
@@ -1,8 +1,30 @@
 package src.main.java.ducdarren.kwic.logic;
 
+import src.main.java.ducdarren.kwic.logic.commands.Command;
 import src.main.java.ducdarren.kwic.logic.commands.CommandResult;
+import src.main.java.ducdarren.kwic.model.LineStorage;
+import src.main.java.ducdarren.kwic.logic.parser.Parser;
+import src.main.java.ducdarren.kwic.model.LineStorageManager;
 
-public interface Logic {
+public class Logic {
 	
-	CommandResult execute(String commandText);
+	private static LineStorage linesInStorage;
+	private static LineStorage circularsStorage;
+	private static Parser parser;
+
+
+    public CommandResult execute(String commandText) {
+        Command command = parser.parseCommand(commandText);
+        command.setStorages(linesInStorage, circularsStorage);
+        CommandResult result = command.execute();
+        // putting valid command into undo stack
+        return result;
+    }
+
+    public static void main(String[] args) {
+    	// init components
+    	linesInStorage = new LineStorageManager();
+        circularsStorage = new LineStorageManager();
+        parser = Parser.getInstance();
+    }
 }
