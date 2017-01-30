f8666bae7a72ec79e8c8a5f5cbd1bbdcaaaa2870
acuodancer <leminhduc74@gmail.com>
2017-01-30 23:07:13 +0800

    Add skeleton for LineStorageManager

---
+++ src/main/java/ducdarren/kwic/model/LineStorageManager.java
@@ -0,0 +1,17 @@
+package src.main.java.ducdarren.kwic.model;
+
+public class LineStorageManager implements LineStorage {
+
+	@Override
+	public void addLine(String line) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void printCirculars() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
