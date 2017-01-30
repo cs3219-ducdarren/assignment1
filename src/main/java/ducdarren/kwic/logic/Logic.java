7f0f20b20ca71243a41d92471461afcc7c837d26
acuodancer <leminhduc74@gmail.com>
2017-01-31 00:12:31 +0800

    Add main logic loop

--- src/main/java/ducdarren/kwic/logic/Logic.java
+++ src/main/java/ducdarren/kwic/logic/Logic.java
@@ -1,5 +1,7 @@
 package main.java.ducdarren.kwic.logic;
 
+import java.util.Scanner;
+
 import main.java.ducdarren.kwic.logic.commands.Command;
 import main.java.ducdarren.kwic.logic.commands.CommandResult;
 import main.java.ducdarren.kwic.model.LineStorage;
@@ -8,12 +10,16 @@ import main.java.ducdarren.kwic.model.LineStorageManager;
 
 public class Logic {
 	
+	private static final String HELP_MESSAGE = "Welcome to KWIC! Available commands: \n"
+			+ "Add: [add title-you-want-to-add]\n"
+			+ "Print: print\n";
+	private static final String SEPARATOR = "====================";
 	private static LineStorage linesInStorage;
 	private static LineStorage circularsStorage;
 	private static Parser parser;
+	private static Scanner scanner;
 
-
-    public CommandResult execute(String commandText) {
+    public static CommandResult execute(String commandText) {
         Command command = parser.parseCommand(commandText);
         command.setStorages(linesInStorage, circularsStorage);
         CommandResult result = command.execute();
@@ -26,5 +32,13 @@ public class Logic {
     	linesInStorage = new LineStorageManager();
         circularsStorage = new LineStorageManager();
         parser = Parser.getInstance();
+        scanner = new Scanner(System.in);
+        // main loop
+        while (true) {
+        	System.out.print(HELP_MESSAGE);
+        	String input = scanner.nextLine();
+        	System.out.println(execute(input).toString());
+        	System.out.println(SEPARATOR);
+        }
     }
 }
