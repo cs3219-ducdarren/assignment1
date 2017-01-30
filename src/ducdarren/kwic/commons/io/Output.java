3bfab8d2096efca0b84611085deb4dfd0cab3d7d
acuodancer <leminhduc74@gmail.com>
2017-01-31 02:59:04 +0800

    Implement print method for output

--- src/ducdarren/kwic/commons/io/Output.java
+++ src/ducdarren/kwic/commons/io/Output.java
@@ -1,11 +1,16 @@
 package ducdarren.kwic.commons.io;
 
+import java.util.LinkedList;
+
 import ducdarren.kwic.implicit.model.TitlesWrapper;
 
 public class Output {
 	
 	public void print(TitlesWrapper circularsList) {
-		
+		LinkedList<String> outputList = circularsList.getAllTitles();
+		for (String s : outputList) {
+			System.out.println(s);
+		}
 	}
 	
 }
