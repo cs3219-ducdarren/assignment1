0414e7b07c640ded4bc67e5ad7f6d997b0cb0cd8
acuodancer <leminhduc74@gmail.com>
2017-01-31 02:56:13 +0800

    Add skeleton output class

---
+++ src/ducdarren/kwic/commons/io/Output.java
@@ -0,0 +1,11 @@
+package ducdarren.kwic.commons.io;
+
+import ducdarren.kwic.implicit.model.TitlesWrapper;
+
+public class Output {
+	
+	public void print(TitlesWrapper circularsList) {
+		
+	}
+	
+}
