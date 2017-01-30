50934c12bd60596382be5002197013f854d8f11e
acuodancer <leminhduc74@gmail.com>
2017-01-31 01:38:46 +0800

    Add helper tokenize and combine methods

--- src/ducdarren/kwic/implicit/model/TitlesList.java
+++ src/ducdarren/kwic/implicit/model/TitlesList.java
@@ -1,21 +1,30 @@
 package ducdarren.kwic.implicit.model;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 
 public class TitlesList {
 	
-	ArrayList<String[]> titles = new ArrayList<String[]>();
+	LinkedList<LinkedList<String>> titles = new LinkedList<LinkedList<String>>();
 	
-	protected void addTitle(String[] title) {
+	protected void addTitle(LinkedList<String> title) {
 		titles.add(title);
 	}
 	
 	protected void addTitle(String title) {
 		this.addTitle(tokenize(title));
 	}
-	
-	private String[] tokenize(String input) {
-		return input.split("\\s+");
+	 
+	private LinkedList<String> tokenize(String input) {
+		return (LinkedList<String>) Arrays.asList(input.split("\\s+"));
 	}
 	
+	private String combine(LinkedList<String> input) {
+		StringBuilder sb = new StringBuilder();
+		for (String s : input) {
+		    sb.append(s);
+		    sb.append("\t");
+		}
+		return sb.toString();
+	}
 }
