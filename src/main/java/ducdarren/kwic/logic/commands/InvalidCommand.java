104bd4a0fc61fadd0884919b82797c59bab35ff9
acuodancer <leminhduc74@gmail.com>
2017-01-30 22:34:48 +0800

    Add InvalidCommand class

---
+++ src/main/java/ducdarren/kwic/logic/commands/InvalidCommand.java
@@ -0,0 +1,14 @@
+package src.main.java.ducdarren.kwic.logic.commands;
+
+public class InvalidCommand extends Command {
+	public final String feedbackToUser;
+
+    public InvalidCommand(String feedbackToUser){
+        this.feedbackToUser = feedbackToUser;
+    }
+
+    @Override
+    public CommandResult execute() {
+        return new CommandResult(feedbackToUser);
+    }
+}
