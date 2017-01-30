a32d96ec3ff138b4aa565edc8e3d3fb383a72bfa
acuodancer <leminhduc74@gmail.com>
2017-01-30 22:49:26 +0800

    Add PrintCommand class

---
+++ src/main/java/ducdarren/kwic/logic/commands/PrintCommand.java
@@ -0,0 +1,19 @@
+package src.main.java.ducdarren.kwic.logic.commands;
+
+public class PrintCommand extends Command {
+	
+	public static final String COMMAND_WORD = "print";
+	public static final String MESSAGE_USAGE = COMMAND_WORD;
+	public static final String MESSAGE_SUCCESS = "Sucessfully printed list of KWIC: ";
+
+	public PrintCommand() {
+		
+	}
+
+	@Override
+	public CommandResult execute() {
+		// need to try/catch duplicate later
+		linesInStorage.printCirculars();
+		return new CommandResult(String.format(MESSAGE_SUCCESS));
+	}
+}
