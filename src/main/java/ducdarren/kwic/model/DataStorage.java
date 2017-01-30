484f3c577f1234a281182a939c8ebb5da973d599
acuodancer <leminhduc74@gmail.com>
2017-01-31 00:39:15 +0800

    Add ArrayList as basic data strcuture

--- src/main/java/ducdarren/kwic/model/DataStorage.java
+++ src/main/java/ducdarren/kwic/model/DataStorage.java
@@ -1,7 +1,11 @@
 package main.java.ducdarren.kwic.model;
 
+import java.util.ArrayList;
+
 public class DataStorage extends DataFacade {
 
+	private ArrayList titles = new ArrayList();
+	
 	@Override
 	public void addLine(String line) {
 		// TODO Auto-generated method stub
