4c68ca6dadb2908c3a5e377d92dc3d9619c3c50c
acuodancer <leminhduc74@gmail.com>
2017-01-31 03:04:41 +0800

    Add I/O to main method logic

--- src/ducdarren/kwic/implicit/KwicImplicitInvocation.java
+++ src/ducdarren/kwic/implicit/KwicImplicitInvocation.java
@@ -1,5 +1,9 @@
 package ducdarren.kwic.implicit;
 
+import java.util.LinkedList;
+
+import ducdarren.kwic.commons.io.Input;
+import ducdarren.kwic.commons.io.Output;
 import ducdarren.kwic.implicit.logic.alphabetizer.Alphabetizer;
 import ducdarren.kwic.implicit.logic.shifter.CircularShifter;
 import ducdarren.kwic.implicit.model.TitlesWrapper;
@@ -8,27 +12,39 @@ public class KwicImplicitInvocation {
 
 	private static TitlesWrapper titles;
 	private static TitlesWrapper circulars;
-	
+	private static LinkedList<String> ignoreList;
+
 	private static CircularShifter circularShifter;
 	private static Alphabetizer alphabetizer;
-	
-	private static void init() {
+
+	private static Input input;
+	private static Output output;
+
+	private static void init(String titlesFile, String ignoreFile) {
+
 		// Databases
 		titles = new TitlesWrapper();
 		circulars = new TitlesWrapper();
-	    // I/O
-		
+		// I/O
+		input = new Input();
+		input.readTitleFile(titlesFile, titles);
+		input.readIgnoreFile(ignoreFile, ignoreList);
+		output = new Output();
 		// Add circularShifter listener in titles
 		circularShifter = new CircularShifter(null, circulars);
-	    titles.addObserver(circularShifter);
-	    // Add alphabetizer listener in circulars
-	    alphabetizer = new Alphabetizer();
-	    circulars.addObserver(alphabetizer);
-	    // 
-	    
+		titles.addObserver(circularShifter);
+		// Add alphabetizer listener in circulars
+		alphabetizer = new Alphabetizer();
+		circulars.addObserver(alphabetizer);
 	}
-	
+
 	public static void main(String[] args) {
-		init();
+		if (args.length != 2) {
+			System.out.println("Error: Invalid number of arguments");
+			System.exit(1);
+		} else {
+			init(args[0], args[1]);
+			output.print(circulars);
+		}
 	}
 }
