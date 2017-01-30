b708dcfa038b9470a328570e77a95488d761540f
acuodancer <leminhduc74@gmail.com>
2017-01-30 21:22:09 +0800

    Add LogicManager class that implements Logic interface

--- src/main/java/ducdarren/kwic/logic/LogicManager.java
+++ src/main/java/ducdarren/kwic/logic/LogicManager.java
@@ -1,15 +1,27 @@
 package src.main.java.ducdarren.kwic.logic;
 
+import src.main.java.ducdarren.kwic.logic.commands.Command;
+import src.main.java.ducdarren.kwic.logic.commands.CommandResult;
 import src.main.java.ducdarren.kwic.model.LineStorage;
 
 public class LogicManager {
 	
 	private final LineStorage linesInStorage;
 	private final LineStorage circularsStorage;
+	private final Parser parser;
 	
 	public LogicManager(LineStorage linesInStorage, LineStorage circularsStorage) {
         this.linesInStorage = linesInStorage;
         this.circularsStorage = circularsStorage;
+    }
+	
+	@Override
+    public CommandResult execute(String commandText) {
+        Command command = parser.parseCommand(commandText);
+        command.setLinesInStorage(linesInStorage);
+        CommandResult result = command.execute();
+        // putting valid command into undo stack
+        return result;
     }
 
 }
